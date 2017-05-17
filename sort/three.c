f7b526be4545733acb430354ce757d1d03536aa1
yujinhai <yujinhai@yongche.com>
2017-05-17 17:42:13 +0800

    three num sort

---
+++ sort/three.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c,temp;
+    printf("input three numers:\n");
+        scanf("%d,%d,%d",&a,&b,&c);
+
+    if ( a > b )
+    {
+        temp=a;
+        a=b;
+        b=temp;
+    }
+
+    if (a > c) 
+    {
+        temp=a;
+        a=c;
+        c=temp;
+    }
+
+    if ( b > c )
+    {
+        temp=b;
+        b=c;
+        c=temp;
+    }
+
+    printf("%d,%d,%d",a,b,c);
+
+    return 1;
+}
