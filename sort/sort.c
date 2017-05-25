2aa4bfa896d77e0ac1fb83058d4a15c8f27599b7
yujinhai <yujinhai@yongche.com>
2017-05-25 18:32:13 +0800

    for sort

--- sort/sort.c
+++ sort/sort.c
@@ -6,7 +6,8 @@ void quick_sort(int s[], int l, int r)
     if (l < r)  
     {  
         int i = l, j = r, x = s[l];  
-
+        int k;
+        
         while (i < j)  
         {  
             while (i < j && s[j] >= x) // 从右向左找第一个小于x的数  
@@ -28,9 +29,15 @@ void quick_sort(int s[], int l, int r)
             {
                 s[j--] = s[i];  
             }
+
         }
 
         s[i] = x;  
+        for ( k=l; k <= r; k++) {
+            printf("%d ", s[k]);
+        }
+
+        printf("\n----------------\n");
         quick_sort(s, l, i - 1); // 递归调用   
         quick_sort(s, i + 1, r);  
     }
@@ -39,10 +46,11 @@ void quick_sort(int s[], int l, int r)
 int main() 
 {
     int i;
-    int a[] = {1, 50, 6, 3, 11111, 45};
+    int a[] = {8, 50, 6, 3, 111, 45};
     quick_sort(a, 0, 5);
+
     for ( i=0; i < 6; i++) {
-        printf("i:%d %d \n",i, a[i]);
+        printf(" %d ",a[i]);
     }
     printf("\n");
 }
